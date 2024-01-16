d719da3eb8a0625e7deeb02d332942ecc411cba4
Lei <caol64@gmail.com>
2024-01-16 18:30:19 +0800

    add post

--- .github/ping_bing.py
+++ .github/ping_bing.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import requests
 import json
 import os
+import sys
 
 
 def get_latest_posts(sitemap_path, n=10):
@@ -24,32 +25,34 @@ def get_latest_posts(sitemap_path, n=10):
     return [url[0] for url in urls[:n]]
 
 
-# Test the function.
-sitemap_path = "../sitemap.xml"  # replace the sitemap file path accordingly
-url_list = get_latest_posts(sitemap_path, 9)
-url_list.insert(0, 'https://babyno.top/')
-print(url_list)
+if __name__ == "__main__":
+    API_KEY = sys.argv[1]
+    TEST = sys.argv[2]
 
-# Prepare the URL and headers.
-url = 'https://www.bing.com/indexnow'
-headers = {
-  'Content-Type': 'application/json; charset=utf-8',
-}
+    sitemap_path = "../sitemap.xml"
+    url_list = get_latest_posts(sitemap_path, 9)
+    url_list.insert(0, 'https://babyno.top/')
+    print(url_list)
 
-API_KEY = os.getenv("BING_API_KEY")
-print(os.getenv("TEST"))
+    # Prepare the URL and headers.
+    url = 'https://www.bing.com/indexnow'
+    headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+    }
 
-# Prepare the body data.
-data = {
-  "host": "babyno.top",
-  "key": API_KEY,
-  "keyLocation": f"https://babyno.top/{API_KEY}.txt",
-  "urlList": url_list
-}
+    print(TEST)
 
-# Send the POST request.
-response = requests.post(url, headers=headers, data=json.dumps(data))
+    # Prepare the body data.
+    data = {
+      "host": "babyno.top",
+      "key": API_KEY,
+      "keyLocation": f"https://babyno.top/{API_KEY}.txt",
+      "urlList": url_list
+    }
 
-# Print the response.
-print(response.status_code)
-print(response.text)
+    # Send the POST request.
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    # Print the response.
+    print(response.status_code)
+    print(response.text)
